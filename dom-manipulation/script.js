c2c5f38e928d8f9fae49344861a3797cba2e1a52
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-03-01 22:27:07 +0100

    commit change

---
+++ dom-manipulation/script.js
@@ -0,0 +1,12 @@
+// Check: Existence of quotes array with correct properties
+let quotes = [
+  {
+    text: "The only way to do great work is to love what you do.",
+    category: "Motivation",
+  },
+  {
+    text: "Innovation distinguishes between a leader and a follower.",
+    category: "Leadership",
+  },
+  { text: "Stay hungry, stay foolish.", category: "Life" },
+];
